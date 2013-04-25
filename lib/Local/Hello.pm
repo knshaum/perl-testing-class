ae55383cbefd565cb6b3d75dde50b027309213ab
Michael G. Schwern <schwern@pobox.com>
2013-04-25 16:23:25 -0700

    If we let 0 work, what about empty string?
    The tests caught us from inadvertantly changing behavior. Now we get to decide
    which behavior we want.

--- lib/Local/Hello.pm
+++ lib/Local/Hello.pm
@@ -3,13 +3,15 @@ package Local::Hello;
 use strict;
 use warnings;
 
+use v5.10;
+
 use parent qw(Exporter);
 our @EXPORT = qw(hello);
 
 sub hello {
     my($greeting, $place) = @_;
-    $greeting ||= "Hello";
-    $place    ||= "world";
+    $greeting //= "Hello";
+    $place    //= "world";
 
     return "$greeting, $place!\n";
 }
